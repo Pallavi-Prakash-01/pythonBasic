960a74c2dc859c944856fa1aea1ff3187d961522
Pallavi-Prakash-01 <84026412+Pallavi-Prakash-01@users.noreply.github.com>
2021-05-12 20:36:37 +0530

    Pallavi | Solution for add two numbers

---
+++ PracticeProblems/AddTwoNumbers.py
@@ -0,0 +1,4 @@
+#problem : Python program to add two numbers
+num1 = float(input("Enter the value for num1 : "))
+num2 = float(input("Enter the value for num2 : "))
+print("the sum is : ", num1+num2)
